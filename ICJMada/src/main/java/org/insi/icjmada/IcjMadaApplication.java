0fb70722ec72d09551c09f58e8bddfb7b57a4297
LucaAntsa <antsalucarabison@gmail.com>
2025-12-18 14:01:10 +0300

    feat: add project spring boot in the repository

---
+++ ICJMada/src/main/java/org/insi/icjmada/IcjMadaApplication.java
@@ -0,0 +1,13 @@
+package org.insi.icjmada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcjMadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcjMadaApplication.class, args);
+    }
+
+}
---
+++ ICJMada/src/test/java/org/insi/icjmada/IcjMadaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.insi.icjmada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcjMadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
